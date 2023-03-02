6a759a22111f7ced052dea8ae77f8713070349a0
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-03-02 10:17:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sesli_sessiz_ayirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cümle Giriniz: ");
+            string cumle = Console.ReadLine().ToLower();
+            char[] sesliler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+            char[] sessizler = { 'b', 'c', 'ç', 'd', 'f', 'g', 'ğ', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 'ş', 't', 'v', 'y', 'z' };
+            int toplam = 0;
+            foreach (char karakter in cumle)
+            {
+                foreach (char sesli in sesliler)
+                {
+                    if (karakter == sesli)
+                    {
+                        toplam++;
+                    }
+                }
+            }
+            foreach (char karakter in cumle)
+            {
+                foreach (char sessiz in sessizler)
+                {
+                    if (karakter == sessiz)
+                    {
+                        toplam++;
+                    }
+                }
+            }
+            Console.WriteLine("Cümlede {0} Tane Sesli Harf Vardır", toplam);
+            Console.ReadLine();
+            Console.WriteLine("Cümlede {0} Tane Sessiz Harf Vardır", toplam);
+            Console.ReadLine();
+        }
+    }
+
+}
